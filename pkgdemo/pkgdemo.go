635fe8d929e7e14d7db24dfde610cbeee91e1a97
yekai1003 <gaoye@tokenpai.com>
2019-08-27 09:50:01 +0800

    a

---
+++ pkgdemo/pkgdemo.go
@@ -0,0 +1,32 @@
+package pkgdemo
+
+import (
+	"fmt"
+)
+
+//外部可导出结构体
+type ExternalPerson struct {
+	Name string //大写，可导出
+	Age  int
+	sex  string //小写，不可导出
+}
+
+//内部不可导出结构体
+type internalPerson struct {
+	Name string
+	Age  int
+	sex  string
+}
+
+func init() {
+	fmt.Println("pkgdemo init is called")
+}
+
+//外部可导出函数
+func NewPerson(name string, age int, sex string) *ExternalPerson {
+	return &ExternalPerson{name, age, sex}
+}
+
+func NewInternalPerson(name string, age int, sex string) *internalPerson {
+	return &internalPerson{name, age, sex}
+}
